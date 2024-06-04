aeeba000367c559a847a92f0d79d1d49ee875c59
odorajbotoj <odoraj_botoj@tom.com>
2024-06-04 19:28:14 +0800

    init project

--- src/showtps/ShowTps.cpp
+++ src/showtps/ShowTps.cpp
@@ -1,34 +1,34 @@
-#include "plugin/MyPlugin.h"
+#include "showtps/showtps.h"
 
 #include <memory>
 
 #include "ll/api/plugin/NativePlugin.h"
 #include "ll/api/plugin/RegisterHelper.h"
 
-namespace my_plugin {
+namespace showtps {
 
-static std::unique_ptr<MyPlugin> instance;
+static std::unique_ptr<ShowTps> instance;
 
-MyPlugin& MyPlugin::getInstance() { return *instance; }
+ShowTps& ShowTps::getInstance() { return *instance; }
 
-bool MyPlugin::load() {
+bool ShowTps::load() {
     getSelf().getLogger().info("Loading...");
     // Code for loading the plugin goes here.
     return true;
 }
 
-bool MyPlugin::enable() {
+bool ShowTps::enable() {
     getSelf().getLogger().info("Enabling...");
     // Code for enabling the plugin goes here.
     return true;
 }
 
-bool MyPlugin::disable() {
+bool ShowTps::disable() {
     getSelf().getLogger().info("Disabling...");
     // Code for disabling the plugin goes here.
     return true;
 }
 
-} // namespace my_plugin
+} // namespace showtps
 
-LL_REGISTER_PLUGIN(my_plugin::MyPlugin, my_plugin::instance);
+LL_REGISTER_PLUGIN(showtps::ShowTps, showtps::instance);
